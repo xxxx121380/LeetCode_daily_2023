81ec440f657eed0839a778cbc8faa83e8f730604
ppy <790031275@qq.com>
2023-10-26 13:22:34 -0700

    fix bugs

--- LeetCode_daily_2023.cpp
+++ LeetCode_daily_2023.cpp
@@ -107,8 +107,9 @@ public:
         {
             if (weight < verticalCuts[i + 1] - verticalCuts[i])weight = verticalCuts[i + 1] - verticalCuts[i];
         }
-        return weight *height % mod;
+        return weight * height % mod;
     };
+};
 int main()
 {
     int punishmentNum[29] = { 1 ,9 ,10 ,36 ,45 ,55 ,82, 91, 99 ,100 ,235, 297, 369 ,370 ,379 ,414, 657 ,675, 703, 756, 792, 909 ,918 ,945 ,964 ,990 ,991 ,999 ,1000 };
